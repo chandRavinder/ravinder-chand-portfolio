356c4325fa5feb478c38c1688bf89f87f6b3d463
LAPTOP-ONF2P9J3\RAVI <ravi.chand.rajput@gmail.com>
2024-03-13 22:04:56 +0530

    Added bootstrap

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import './navbar.css';
-// import logo from '../../assets/logo.png';
 import profile from '../../assets/Ravinder-Profile.png'
-import contactImg from '../../assets/contact.png'
+// import contactImg from '../../assets/contact.png'
 import { Link } from 'react-scroll';
 
 export const Navbar = () => {
   return (
-    <nav className='navbar'>
+    // <nav className='navbar'>
+    //     <img src={profile} alt='LogoImage' className='logo' />
+
+    //     <div className='desktopMenu'>
+    //         <Link className="desktopListItem">Home</Link>
+    //         <Link className="desktopListItem">About</Link>
+    //     </div>
+
+    //     <button className='desktopMenuBtn'>
+    //         <img src={contactImg} alt='ContactImage' className='desktopMenuImg' />Contact Me
+    //     </button>
+    // </nav>
+    <nav className="navbar navbar-expand-sm bg-light justify-content-center">
         <img src={profile} alt='LogoImage' className='logo' />
 
-        <div className='desktopMenu'>
-            <Link className="desktopListItem">Home</Link>
-            <Link className="desktopListItem">Clients</Link>
-            <Link className="desktopListItem">About</Link>
-            <Link className="desktopListItem">Portfolio</Link>
-        </div>
-
-        <button className='desktopMenuBtn'>
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <Link className="desktopListItem" to=''>Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="desktopListItem" to=''>About</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="desktopListItem" to=''>Contact</Link>
+            </li>
+        </ul>
+        
+        {/* <button className='desktopMenuBtn'>
             <img src={contactImg} alt='ContactImage' className='desktopMenuImg' />Contact Me
-        </button>
+        </button> */}
+        
     </nav>
   )
 }

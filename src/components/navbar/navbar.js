299dfb298ccad8e75899e1e50b6569e179f15531
LAPTOP-ONF2P9J3\RAVI <ravi.chand.rajput@gmail.com>
2024-07-02 12:40:42 +0530

    Working on navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,44 +1,45 @@
 import React from 'react';
 import './navbar.css';
-import profile from '../../assets/Ravinder-Profile.png'
+// import profile from '../../assets/Ravinder-Profile.png'
 // import contactImg from '../../assets/contact.png'
 import { Link } from 'react-scroll';
 
 export const Navbar = () => {
-  return (
-    // <nav className='navbar'>
-    //     <img src={profile} alt='LogoImage' className='logo' />
-
-    //     <div className='desktopMenu'>
-    //         <Link className="desktopListItem">Home</Link>
-    //         <Link className="desktopListItem">About</Link>
-    //     </div>
-
-    //     <button className='desktopMenuBtn'>
-    //         <img src={contactImg} alt='ContactImage' className='desktopMenuImg' />Contact Me
-    //     </button>
-    // </nav>
-    <nav className="navbar navbar-expand-sm bg-light justify-content-center">
-        <img src={profile} alt='LogoImage' className='logo' />
-
-        <ul className="navbar-nav">
-            <li className="nav-item">
-                <Link className="desktopListItem" to=''>Home</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="desktopListItem" to=''>About</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="desktopListItem" to=''>Contact</Link>
-            </li>
-        </ul>
-        
-        {/* <button className='desktopMenuBtn'>
-            <img src={contactImg} alt='ContactImage' className='desktopMenuImg' />Contact Me
-        </button> */}
-        
-    </nav>
-  )
+    return (
+        <nav className="navbar navbar-expand-sm justify-content-around">
+            {/* <img src={profile} alt='LogoImage' className='logo' /> */}
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <p className='my-name'>Ravinder Chand</p>
+                </li>
+            </ul>
+                
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="desktopListItem" to=''>Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="desktopListItem" to=''>About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="desktopListItem" to=''>Certification</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="desktopListItem" to=''>Contact</Link>
+                </li>
+                <li className="nav-item">
+                    {/* <button className='hire-me-button'> */}
+                        <Link className="desktopListItem" to=''>Hire Me</Link>
+                    {/* </button> */}
+                </li>
+            </ul>
+            
+            {/* <button className='desktopMenuBtn'>
+                <img src={contactImg} alt='ContactImage' className='desktopMenuImg' />Contact Me
+            </button> */}
+            
+        </nav>
+    )
 }
 
-export default Navbar
+export default Navbar;
